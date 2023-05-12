6d634ea585afadd5f42fa50b1d372e8827b27445
Trong Hieu Vuong <hieuvg02@gmail.com>
2023-05-12 14:34:06 -0700

    Fixing the file to handle file without assuming the number of lines.

--- a0/word_count.c
+++ a0/word_count.c
@@ -1,36 +1,41 @@
 #include<stdio.h>
 #include<string.h>
 #include <stdlib.h>
-#define MAX_LINE 100
-#define MAX_LINE_LEN 110
-#define MAX_WORD 500
-#define MAX_WORD_LEN 120
 int main(int argc, char *argv[]) {
-    char buffer[MAX_LINE][MAX_LINE_LEN];
-    char f1[MAX_WORD][MAX_WORD];
     int num_lines=0;
+
+    // Check whether file is input.
     if(argc<2){
         fprintf(stderr, "You should provide a filename\n");
         exit(1);
     }
-    FILE *data_fp = fopen(argv[1], "r");
-    for(int i= 0; i<MAX_LINE; i++){
-            while(fgets(buffer[i], MAX_LINE_LEN, data_fp)){
-                buffer[i][strlen(buffer[i])-1]= '\0';
-                strncpy(f1[num_lines], buffer[i], MAX_WORD_LEN); 
-                num_lines++;
-            }
-    }
     int count = 0;
-    for(int i = 0; i<num_lines; i++) {
-        char* elements = strtok(f1[i], " ");
-        while (elements != NULL){
-            // printf("%s\n", elements);
-            elements = strtok(NULL, " ");
-            count++;
-        }
+    char ch;
+
+    // Open the file to count the number of lines
+    FILE *data_fp = fopen(argv[1], "r");
+    if (NULL == data_fp) {
+        printf("file can't be opened \n");
     }
-    printf("%d %s", count, argv[1]);
+ 
+    // Count the number of lines in the file
+    do {
+        ch = fgetc(data_fp);
+        if (ch == '\n') {
+            num_lines++;
+        }
+    } while (ch != EOF);
+    fclose(data_fp);
+
+    // Open the file again and count the number of words in there
+    data_fp = fopen(argv[1], "r");
+    char buffer[num_lines];
+    while (fscanf(data_fp, "%s", buffer) != EOF) {
+        count++;
+    }
+    printf("%d %s\n", count, argv[1]);
+
+    // Close the file
     fclose(data_fp);
     return 0;
 }
